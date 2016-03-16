865f8f28f64bbe45cf8cc9799d6101ac1aaa5e5a
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-03-16 13:01:20 +0200

    Updated API routes.

--- js/services.js
+++ js/services.js
@@ -14,36 +14,36 @@ angular.module('app.services', [])
 
 // Create get factory to retrieve data from api
 	portfolioFactory.all = function(){
-		return	$http.get('http://localhost:3000/api/portfolio/');
+		return	$http.get('http://boiling-mesa-13925.herokuapp.com/api/portfolio/');
 	}
 
 	// Create get factory to retrieve data from api
 	portfolioFactory.latest = function(){
-		return	$http.get('http://localhost:3000/api/portfolio/?query=latest');
+		return	$http.get('http://boiling-mesa-13925.herokuapp.com/api/portfolio/?query=latest');
 	}
 
 	// Create get factory to retrieve data from api
 	portfolioFactory.previous = function(){
-		return	$http.get('http://localhost:3000/api/portfolio/?query=previous');
+		return	$http.get('http://boiling-mesa-13925.herokuapp.com/api/portfolio/?query=previous');
 	}
 
 // Create get factory to retrieve data from api
 	portfolioFactory.newsletter = function(){
-		return	$http.get('http://localhost:3000/api/portfolio/?query=newsletter');
+		return	$http.get('http://boiling-mesa-13925.herokuapp.com/api/portfolio/?query=newsletter');
 	}
 
 // Create get factory to retrieve data from api
 	portfolioFactory.landingpages = function(){
-		return	$http.get('http://localhost:3000/api/portfolio/?query=landingpage');
+		return	$http.get('http://boiling-mesa-13925.herokuapp.com/api/portfolio/?query=landingpage');
 	}
 
 	// Create get factory to retrieve data from api
 	portfolioFactory.other = function(){
-		return	$http.get('http://localhost:3000/api/portfolio/?query=other');
+		return	$http.get('http://boiling-mesa-13925.herokuapp.com/api/portfolio/?query=other');
 	}
 // Create get factory to retrieve data from api
 	portfolioFactory.recent = function(){
-		return	$http.get('http://localhost:3000/api/portfolio/?query=recent');
+		return	$http.get('http://boiling-mesa-13925.herokuapp.com/api/portfolio/?query=recent');
 	}
 
 
